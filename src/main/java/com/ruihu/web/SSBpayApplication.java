dc098eb2237880215df372aa775cba0cc83a8d92
RUI HU <rayhusme@gmail.com>
2017-04-01 02:07:18 -0600

    Modify index base on thymeleaf

---
+++ src/main/java/com/ruihu/web/SSBpayApplication.java
@@ -0,0 +1,16 @@
+package com.ruihu.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by Administrator on 2017/4/1.
+ */
+@SpringBootApplication
+public class SSBpayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SSBpayApplication.class, args);
+    }
+
+}
