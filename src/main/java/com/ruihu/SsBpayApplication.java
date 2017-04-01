b7f2b792b547241a1d9a5ad394bfb9f89938cc72
RUI HU <rayhusme@gmail.com>
2017-03-31 20:11:06 -0600

    Initial SSBpay

---
+++ src/main/java/com/ruihu/SsBpayApplication.java
@@ -0,0 +1,12 @@
+package com.ruihu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsBpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsBpayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ruihu/SsBpayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ruihu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsBpayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
