7f937d1b803b3533ac31c41de46249ae063129de
RUI HU <rayhusme@gmail.com>
2017-04-03 01:06:22 -0600

    Added log fileds and I18Nservice

--- src/main/java/com/ruihu/web/i18n/I18NService.java
+++ src/main/java/com/ruihu/web/i18n/I18NService.java
@@ -1,6 +1,8 @@
 package com.ruihu.web.i18n;
 
 import org.apache.tomcat.jni.Local;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -11,10 +13,13 @@ import java.util.Locale;
  */
 @Service
 public class I18NService {
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
+
     @Autowired
     private MessageSource messageSource;
 
     public String getMessage(String messageId){
+        LOG.info("ssssssssssssssssssssssss {}",messageId);
         Locale local = LocaleContextHolder.getLocale();
         return getMessage(messageId,local);
     }
--- src/test/java/com/ruihu/SsBpayApplicationTests.java
+++ src/test/java/com/ruihu/SsBpayApplicationTests.java
@@ -1,16 +1,25 @@
 package com.ruihu;
 
+import com.ruihu.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SsBpayApplicationTests {
+	@Autowired
+	private I18NService i18NService;
 
 	@Test
 	public void contextLoads() {
+		String e ="s";
+		String o="2";
+		String ww = i18NService.getMessage(o);
+		Assert.assertEquals("",e,ww);
 	}
 
 }
